7527c09fb358114811b3fb6cf89ae0cb32bf82d6
wouter bolsterlee <wouter@bolsterl.ee>
2019-02-06 13:20:42 +0100

    add more metadata

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name="black-macchiato",
     description="Partial black formatting",
+    url="https://github.com/wbolster/black-macchiato",
     author="wouter bolsterlee",
     author_email="wouter@bolsterl.ee",
     license="BSD License",
-    version="1.0.0",
+    version="1.0.1",
     py_modules=["macchiato"],
     install_requires=["black"],
     python_requires=">=3.6",
